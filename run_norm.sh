b59822011448d0a105278e0b28fc45c5efc39ad1
XLP <qlinsey@users.noreply.github.com>
2024-09-06 13:36:38 -0700

    Add files via upload

---
+++ run_norm.sh
@@ -0,0 +1,185 @@
+echo lmd:0.01 sigma: 0.2 norm: true
+python run.py --templates train_Mixtime --lmd 0.01 --sigma 0.2 --norm True --experiment_root experiments_cara_ml_1m_lmd001_sigma02_norm_true
+echo 'done Mixtime'
+
+
+echo lmd:0.01 sigma: 0.2 norm: false
+python run.py --templates train_Mixtime --lmd 0.01 --sigma 0.2 --norm False --experiment_root experiments_cara_ml_1m_lmd001_sigma02_norm_false
+echo 'done Mixtime'
+
+echo lmd:0.02 sigma: 0.2 norm: true
+python run.py --templates train_Mixtime --lmd 0.02 --sigma 0.2 --norm True --experiment_root experiments_cara_ml_1m_lmd002_sigma02_norm_true 
+echo 'done Mixtime'
+
+
+echo lmd:0.02 sigma: 0.2 norm: false
+python run.py --templates train_Mixtime --lmd 0.02 --sigma 0.2 --norm False --experiment_root experiments_cara_ml_1m_lmd002_sigma02_norm_false
+echo 'done Mixtime'
+
+echo lmd:0.04 sigma: 0.2 norm: true
+python run.py --templates train_Mixtime --lmd 0.04 --sigma 0.2 --norm True --experiment_root experiments_cara_ml_1m_lmd004_sigma02_norm_true
+
+
+echo lmd:0.04 sigma: 0.2 norm: false
+python run.py --templates train_Mixtime --lmd 0.04 --sigma 0.2 --norm False --experiment_root experiments_cara_ml_1m_lmd004_sigma02_norm_false
+
+
+echo lmd:0.08 sigma: 0.2 norm: true
+python run.py --templates train_Mixtime --lmd 0.08 --sigma 0.2 --norm True --experiment_root experiments_cara_ml_1m_lmd008_sigma02_norm_true
+
+
+echo lmd:0.08 sigma: 0.2 norm: false
+python run.py --templates train_Mixtime --lmd 0.08 --sigma 0.2 --norm False --experiment_root experiments_cara_ml_1m_lmd008_sigma02_norm_false
+
+echo lmd:0.1 sigma: 0.2 norm: true
+python run.py --templates train_Mixtime --lmd 0.1 --sigma 0.2 --norm True --experiment_root experiments_cara_ml_1m_lmd01_sigma02_norm_true
+
+
+echo lmd:0.1 sigma: 0.2 norm: false
+python run.py --templates train_Mixtime --lmd 0.1 --sigma 0.2 --norm False --experiment_root experiments_cara_ml_1m_lmd01_sigma02_norm_false
+
+echo lmd:0.02 sigma: 0.1 norm: true
+python run.py --templates train_Mixtime --lmd 0.02 --sigma 0.1 --norm True --experiment_root experiments_cara_ml_1m_lmd002_sigma01_norm_true
+echo 'done Mixtime'
+
+echo lmd:0.02 sigma: 0.1 norm: false
+python run.py --templates train_Mixtime --lmd 0.02 --sigma 0.1 --norm False --experiment_root experiments_cara_ml_1m_lmd002_sigma01_norm_false
+echo 'done Mixtime'
+
+echo lmd:0.04 sigma: 0.1 norm: true
+python run.py --templates train_Mixtime --lmd 0.04 --sigma 0.1 --norm True --experiment_root experiments_cara_ml_1m_lmd004_sigma01_norm_true
+
+
+echo lmd:0.04 sigma: 0.1 norm: false
+python run.py --templates train_Mixtime --lmd 0.04 --sigma 0.1 --norm False --experiment_root experiments_cara_ml_1m_lmd004_sigma01_norm_false
+
+
+echo lmd:0.08 sigma: 0.1 norm: true
+python run.py --templates train_Mixtime --lmd 0.08 --sigma 0.1 --norm True --experiment_root experiments_cara_ml_1m_lmd008_sigma01_norm_true
+
+
+echo lmd:0.08 sigma: 0.1 norm: false
+python run.py --templates train_Mixtime --lmd 0.08 --sigma 0.1 --norm False --experiment_root experiments_cara_ml_1m_lmd008_sigma01_norm_false
+
+echo lmd:0.1 sigma: 0.1 norm: true
+python run.py --templates train_Mixtime --lmd 0.1 --sigma 0.1 --norm True --experiment_root experiments_cara_ml_1m_lmd01_sigma01_norm_true
+
+
+echo lmd:0.1 sigma: 0.1 norm: false
+python run.py --templates train_Mixtime --lmd 0.1 --sigma 0.1 --norm False --experiment_root experiments_cara_ml_1m_lmd01_sigma01_norm_false
+
+echo lmd:0.01 sigma: 0.2 norm: true
+python run.py --templates train_Mixtime --lmd 0.01 --sigma 0.2 --norm True --experiment_root experiments_cara_ml_1m_lmd001_sigma02_norm_true
+echo 'done Mixtime'
+
+echo lmd:0.01 sigma: 0.2 norm: false
+python run.py --templates train_Mixtime --lmd 0.01 --sigma 0.2 --norm False --experiment_root experiments_cara_ml_1m_lmd001_sigma02_norm_false
+echo 'done Mixtime'
+
+echo lmd:0.02 sigma: 0.2 norm: true
+python run.py --templates train_Mixtime --lmd 0.02 --sigma 0.2 --norm True --experiment_root experiments_cara_ml_1m_lmd002_sigma02_norm_true
+echo 'done Mixtime'
+
+
+echo lmd:0.02 sigma: 0.2 norm: false
+python run.py --templates train_Mixtime --lmd 0.02 --sigma 0.2 --norm False --experiment_root experiments_cara_ml_1m_lmd002_sigma02_norm_false
+echo 'done Mixtime'
+
+
+echo lmd:0.04 sigma: 0.2 norm: true
+python run.py --templates train_Mixtime --lmd 0.04 --sigma 0.2 --norm True --experiment_root experiments_cara_ml_1m_lmd004_sigma02_norm_true
+
+echo lmd:0.04 sigma: 0.2 norm: false
+python run.py --templates train_Mixtime --lmd 0.04 --sigma 0.2 --norm False --experiment_root experiments_cara_ml_1m_lmd004_sigma02_norm_false
+
+echo lmd:0.08 sigma: 0.2 norm: true
+python run.py --templates train_Mixtime --lmd 0.08 --sigma 0.2 --norm True --experiment_root experiments_cara_ml_1m_lmd008_sigma02_norm_true
+
+
+echo lmd:0.08 sigma: 0.2 norm: false
+python run.py --templates train_Mixtime --lmd 0.08 --sigma 0.2 --norm False --experiment_root experiments_cara_ml_1m_lmd008_sigma02_norm_false
+
+
+echo lmd:0.1 sigma: 0.2 norm: true
+python run.py --templates train_Mixtime --lmd 0.1 --sigma 0.2 --norm True --experiment_root experiments_cara_ml_1m_lmd01_sigma02_norm_true
+
+
+echo lmd:0.1 sigma: 0.2 norm: false
+python run.py --templates train_Mixtime --lmd 0.1 --sigma 0.2 --norm False --experiment_root experiments_cara_ml_1m_lmd01_sigma02_norm_false
+
+echo lmd:0.01 sigma: 0.2 norm: true
+python run.py --templates train_Mixtime --lmd 0.01 --sigma 0.2 --norm True --experiment_root experiments_cara_ml_1m_lmd001_sigma02_norm_true
+echo 'done Mixtime'
+
+
+echo lmd:0.01 sigma: 0.2 norm: false
+python run.py --templates train_Mixtime --lmd 0.01 --sigma 0.2 --norm False --experiment_root experiments_cara_ml_1m_lmd001_sigma02_norm_false
+echo 'done Mixtime'
+
+echo lmd:0.02 sigma: 0.2 norm: true
+python run.py --templates train_Mixtime --lmd 0.02 --sigma 0.2 --norm True --experiment_root experiments_cara_ml_1m_lmd002_sigma02_norm_true
+echo 'done Mixtime'
+
+echo lmd:0.02 sigma: 0.2 norm: false
+python run.py --templates train_Mixtime --lmd 0.02 --sigma 0.2 --norm False --experiment_root experiments_cara_ml_1m_lmd002_sigma02_norm_false
+echo 'done Mixtime'
+
+
+echo lmd:0.04 sigma: 0.2 norm: true
+python run.py --templates train_Mixtime --lmd 0.04 --sigma 0.2 --norm True --experiment_root experiments_cara_ml_1m_lmd004_sigma02_norm_true
+
+
+echo lmd:0.04 sigma: 0.2 norm: false
+python run.py --templates train_Mixtime --lmd 0.04 --sigma 0.2 --norm False --experiment_root experiments_cara_ml_1m_lmd004_sigma02_norm_false
+
+
+echo lmd:0.08 sigma: 0.2 norm: true
+python run.py --templates train_Mixtime --lmd 0.08 --sigma 0.2 --norm True --experiment_root experiments_cara_ml_1m_lmd008_sigma02_norm_true
+
+echo lmd:0.08 sigma: 0.2 norm: false
+python run.py --templates train_Mixtime --lmd 0.08 --sigma 0.2 --norm False --experiment_root experiments_cara_ml_1m_lmd008_sigma02_norm_false
+
+echo lmd:0.1 sigma: 0.2 norm: true
+python run.py --templates train_Mixtime --lmd 0.1 --sigma 0.2 --norm True --experiment_root experiments_cara_ml_1m_lmd01_sigma02_norm_true
+
+
+echo lmd:0.1 sigma: 0.2 norm: false
+python run.py --templates train_Mixtime --lmd 0.1 --sigma 0.2 --norm False --experiment_root experiments_cara_ml_1m_lmd01_sigma02_norm_false
+
+echo lmd:0.01 sigma: 1 norm: true
+python run.py --templates train_Mixtime --lmd 0.01 --sigma 1 --norm True --experiment_root experiments_cara_ml_1m_lmd001_sigma1_norm_true
+echo 'done Mixtime'
+
+echo lmd:0.01 sigma: 1 norm: false
+python run.py --templates train_Mixtime --lmd 0.01 --sigma 1 --norm False --experiment_root experiments_cara_ml_1m_lmd001_sigma1_norm_false
+echo 'done Mixtime'
+
+echo lmd:0.02 sigma: 1 norm: true
+python run.py --templates train_Mixtime --lmd 0.02 --sigma 1 --norm True --experiment_root experiments_cara_ml_1m_lmd002_sigma1_norm_true
+echo 'done Mixtime'
+
+
+echo lmd:0.02 sigma: 1 norm: false
+python run.py --templates train_Mixtime --lmd 0.02 --sigma 1 --norm False --experiment_root experiments_cara_ml_1m_lmd002_sigma1_norm_false
+echo 'done Mixtime'
+
+echo lmd:0.04 sigma: 1 norm: true
+python run.py --templates train_Mixtime --lmd 0.04 --sigma 1 --norm True --experiment_root experiments_cara_ml_1m_lmd004_sigma1_norm_true
+
+
+echo lmd:0.04 sigma: 1 norm: false
+python run.py --templates train_Mixtime --lmd 0.04 --sigma 1 --norm False --experiment_root experiments_cara_ml_1m_lmd004_sigma1_norm_false
+
+echo lmd:0.08 sigma: 1 norm: true
+python run.py --templates train_Mixtime --lmd 0.08 --sigma 1 --norm True --experiment_root experiments_cara_ml_1m_lmd008_sigma1_norm_true
+
+echo lmd:0.08 sigma: 1 norm: False
+python run.py --templates train_Mixtime --lmd 0.08 --sigma 1 --norm False --experiment_root experiments_cara_ml_1m_lmd008_sigma1_norm_false
+
+echo lmd:0.1 sigma: 1 norm: true
+python run.py --templates train_Mixtime --lmd 0.1 --sigma 1 --norm True --experiment_root experiments_cara_ml_1m_lmd01_sigma1_norm_true
+
+echo lmd:0.1 sigma: 1 norm: false
+python run.py --templates train_Mixtime --lmd 0.1 --sigma 1 --norm False --experiment_root experiments_cara_ml_1m_lmd01_sigma1_norm_false
+
+~
